cd30cc52acccbc582c20b86c2127cef91fbc8b87
MichaelMantovani <michmantovani@gmail.com>
2023-05-31 16:51:52 +0200

    Complete email exercise

--- js/email.js
+++ js/email.js
@@ -7,3 +7,35 @@ const emails = ['mariorossi@gmail.com', 'paolobranbilla23@gmail.com', 'paolalomb
 const userEmailInput = document.getElementById('user-email');
 const submitButton = document.getElementById('btn-submit');
 const userGreeting = document.getElementById('welcome')
+
+
+// Creo un messaggio di benvenuto 
+const grettingMessage = 'Benvenuto, la tua mail è'
+
+// Mi metto in ascolto del click del bottone
+  submitButton.addEventListener('click', function() {
+  
+  // recupero il valore dell'input
+  const userEmail = userEmailInput.value.trim();
+
+  // Controllo se l'input è una mail valida
+  if (userEmail.length < 1){
+  alert('Dati inseriti non validi')
+  } else {
+
+    // Controllo tutte le email salvate
+    for (i = 0; i < emails.length; i++) {
+
+      // Se trovo una email uguale creo un alert
+      if (userEmail === emails [i]) {
+        alert("Email inserita già esistente, per favore scegliere un'altra email")
+      }
+    }
+
+
+    // Stampo il messaggio di benvenuto in pagina
+    userGreeting.innerText = grettingMessage + ' ' + userEmail;
+  }
+  
+  
+})
