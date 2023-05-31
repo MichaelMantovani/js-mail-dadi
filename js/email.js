6a04832f141e6df9438bc0f5a3bc09275e268f6b
MichaelMantovani <michmantovani@gmail.com>
2023-05-31 15:42:52 +0200

    Create email array and collect elements from DOM

---
+++ js/email.js
@@ -0,0 +1,9 @@
+console.log('JS OK');
+
+// Creo un array con delle mail
+const emails = ['mariorossi@gmail.com', 'paolobranbilla23@gmail.com', 'paolalombardi@gmail.com', 'martasiciliano@gmail.com', 'ramirobarese@gmail.com', 'sandropagnotto1960@gmail.com'];
+
+// Recupero gl elementi dal DOM
+const userEmailInput = document.getElementById('user-email');
+const submitButton = document.getElementById('btn-submit');
+const userGreeting = document.getElementById('welcome')
